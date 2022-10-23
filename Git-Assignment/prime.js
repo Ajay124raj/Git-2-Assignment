f83714f99c6f384c3f8949e7683b8815602a3752
rishab <rkc3660@gmail.com>
2022-10-23 21:56:53 +0530

    Correct code

--- Git-Assignment/prime.js
+++ Git-Assignment/prime.js
@@ -1,10 +1,10 @@
 let check_prime = 13;
 let factor = 0;
 
-for (let i = 0; i <= check_prime; i++) {
+for (let i = 1; i <= check_prime; i++) {
 
   check_prime % i == 0 ? factor++ : "";
 
 }
 
-factor == 5 ? console.log("Yes") : console.log("No");
+factor == 2 ? console.log("Yes") : console.log("No");
